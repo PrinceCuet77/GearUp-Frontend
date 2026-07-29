17325c367f4e940fe738244feb1a035f6f5cbb28
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2026-07-29 20:34:35 +0600

    feat: next.js inital project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Toaster } from 'sonner';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'GearUp: Rent Sports & Outdoor Gear',
+  description:
+    'Browse and rent premium sports and outdoor equipment. Find bikes, tents, kayaks, and more.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang='en'
+      suppressHydrationWarning
+      className={`${geistSans.variable} ${geistMono.variable}`}
+    >
+      <body
+        suppressHydrationWarning
+        className='min-h-screen flex flex-col antialiased'
+      >
+        <Toaster position='top-right' richColors closeButton />
+        <main className='flex-1'>{children}</main>
+      </body>
+    </html>
+  );
+}
